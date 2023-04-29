12d61c524b1d446d39f522f5aff865ab6a6f5dc8
GxbrielTxrres <gtorres2204@outlook.com>
2023-04-29 14:09:16 -0500

    's'

--- components/Experience.js
+++ components/Experience.js
@@ -28,11 +28,39 @@ export default function Experience() {
 		tl.current.to(
 			camera.rotation,
 			{
-				y: -Math.PI,
+				y: -Math.PI / 2,
 				duration: 0.5,
 				ease: "power3.inOut",
 			},
 			0.9,
+		);
+
+		tl.current.to(
+			ref.current.rotation,
+			{
+				x: Math.sin(Math.PI * 2),
+				y: Math.cos(Math.PI * 2),
+				duration: 0.5,
+			},
+			1.5,
+		);
+
+		tl.current.to(
+			ref.current.position,
+			{
+				y: 1,
+				duration: 1,
+			},
+			1.5,
+		);
+
+		tl.current.to(
+			camera.position,
+			{
+				y: -5,
+				duration: 2,
+			},
+			1.5,
 		);
 	}, []);
 
@@ -48,7 +76,10 @@ export default function Experience() {
 			<Effects />
 			<color args={["#ffffff"]} attach={"background"} />
 			<Environment preset="night" />
-
+			<mesh ref={ref} position={[10, 0, 5]}>
+				<boxGeometry />
+				<meshStandardMaterial />
+			</mesh>
 			<Masks />
 		</>
 	);
